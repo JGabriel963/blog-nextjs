c88e28a817654aa8f6c5cdf4a593e88de6d23c92
João Gabriel <joaogabriel9633@gmail.com>
2025-07-19 19:44:37 -0300

    chore: add seo inn blog list page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,11 +1,34 @@
-import { BlogList } from '@/templates/blog'
-import { allPosts } from 'contentlayer/generated'
-import React from 'react'
+import { BlogList } from "@/templates/blog";
+import { allPosts } from "contentlayer/generated";
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Blog",
+  description: "Dicas e estratégias para impulsionar seu negócio",
+  robots: "index, follow",
+  openGraph: {
+    title: "Blog",
+    description: "Venda seus produtos como afiliado em um único lugar",
+    url: "https://blog-nextjs-henna-six.vercel.app/og-image.jpg",
+    siteName: "Site.Set",
+    locale: "pt_BR",
+    type: "website",
+    images: [
+      {
+        url: "https://blog-nextjs-henna-six.vercel.app/og-image.jpg",
+        width: 800,
+        height: 600,
+        alt: "Site.Set",
+      },
+    ],
+  },
+};
 
 export default function BlogListPage() {
-    const sortedPosts  = allPosts.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+  const sortedPosts = allPosts.sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+  );
 
-  return (
-    <BlogList posts={sortedPosts} />
-  )
+  return <BlogList posts={sortedPosts} />;
 }
